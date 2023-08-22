adfcbd8fdb64db9a003893dd95c8569e66f48803
myingineer <e.alexander190@yahoo.com>
2023-08-22 14:33:47 +0100

    finished-version

---
+++ script.js
@@ -0,0 +1,38 @@
+// Calling the Button
+const clickMeBtn = document.querySelector('.btn');
+
+// Calling all the Displays in the HTML
+const participantsDisplay = document.querySelector('.js-participants');
+const typeDisplay = document.querySelector('.js-type');
+const activityDisplay = document.querySelector('.js-activity');
+const accessibilityDisplay = document.querySelector('.js-accessibility');
+const priceDisplay = document.querySelector('.js-price');
+const linkDisplay = document.querySelector('.js-link');
+
+// The Api Url
+const apiLink = "https://www.boredapi.com/api/activity";
+
+
+/*
+    This function calls the API and displays it in its respective field
+*/
+
+async function generateBoredApi() {
+    try {
+        const request = await fetch(apiLink);
+        let response = await request.json();
+        participantsDisplay.innerText = response.participants;
+        typeDisplay.innerText = response.type;
+        activityDisplay.innerText = response.activity;
+        accessibilityDisplay.innerText = response.accessibility;
+        priceDisplay.innerText = response.price;
+        linkDisplay.innerText = response.link;
+        console.log(response);
+    } catch (error) {
+        console.log(`We ran into a problem check back later ${error}`);
+    };
+};
+
+clickMeBtn.addEventListener('click', () => {
+    generateBoredApi();
+});
